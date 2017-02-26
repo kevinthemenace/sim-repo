746fb0b4b30edf3ca5c2de19f5b2fbcba41cd945
kevinthemenace <john_kevin@zoho.com>
2017-02-26 21:35:26 +0300

    finishing touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 var links = document.querySelectorAll('a');
 
 for (var i=0; i<links.length; i++) {
-	links[i].classname = 'kevlink';
+	links[i].className = "kevlink";
 	links[i].addEventListener('mouseenter', enlargeText);
 	links[i].addEventListener('mouseleave', initialText);
 }
 
 function enlargeText() {
-	this.classname = 'enlarge';
+	this.className = "enlarge";
 }
 
 function initialText() {
-	this.classname = 'initialSize';
+	this.className = "initialSize";
 }
