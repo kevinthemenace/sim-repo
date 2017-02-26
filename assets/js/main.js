1cf6165e6a293ba071d133dfb2465775a5917378
kevinthemenace <john_kevin@zoho.com>
2017-02-26 20:49:59 +0300

    added more js functionality, css classes remain to be created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 var links = document.querySelectorAll('a');
+
+for (var i=0; i<links.length; i++) {
+	links[i].classname = 'kevlink';
+	links[i].addEventListener(mouseenter, enlargeText);
+	links[i].addEventListener(mouseleave, initialText);
+}
+
+function enlargeText() {
+	for (var i=0; i<links.length; i++) {
+		links[i].classname = 'enlarge';
+	}
+}
+
+function initialText() {
+	for (var i=0; i<links.length; i++) {
+		links[i].classname = 'initialText';
+	}
+}
